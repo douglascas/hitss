4142341079bc77bbff59161c660e767c4708858f
Douglas Sena <douglas.sena@mv.com.br>
2018-04-19 14:32:29 -0300

    Implementações na lista de celulares

--- src/app/cellphone-list/cellphone-list.module.ts
+++ src/app/cellphone-list/cellphone-list.module.ts
@@ -1,15 +1,29 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { CellphoneListComponent } from './cellphone-list.component';
 import { CellphoneComponent } from './cellphone.component';
 import { RouterModule } from '@angular/router';
+import { MatInputModule, MatIconModule, MatGridListModule } from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
     RouterModule,
+    MatInputModule,
+    MatIconModule,
+    FlexLayoutModule,
+    FormsModule,
+    MatGridListModule,
   ],
   exports: [CellphoneListComponent],
   declarations: [CellphoneListComponent, CellphoneComponent],
 })
 export class CellphoneListModule { }
+
--- src/app/cellphone-list/cellphone.component.ts
+++ src/app/cellphone-list/cellphone.component.ts
@@ -1,3 +1,5 @@
+import { CellphoneDialogComponent } from './../cellphone-dialog/cellphone-dialog.component';
+import { MatDialog } from '@angular/material';
 import { Component, OnInit, Input } from '@angular/core';
 import { Cellphone } from '../shared/index';
 
@@ -8,7 +10,17 @@ import { Cellphone } from '../shared/index';
 })
 export class CellphoneComponent {
 
+
   @Input()
   cellphone: Cellphone;
+
+  constructor(
+    private readonly _dialog: MatDialog,
+
+  ) {}
+
+  photoDetail() {
+    this._dialog.open(CellphoneDialogComponent, {data: this.cellphone.model});
+  }
 
 }
