1f04b01cceef57fbcbc2cd970d44e8e0ab930ce6
douglas.sena <douglascas@hotmail.com>
2018-04-18 21:00:22 -0300

    Iniciando projeto Hitss

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('hitss-project App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
